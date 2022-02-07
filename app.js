01425f127e9c8bbf0b9d3d219be7126e47b0eff1
aravindkol <97201015+aravindkol@users.noreply.github.com>
2022-02-06 18:13:14 -0800

    Task 5 (#3)
    * Update app.js
    
    * Update swagger.yaml

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 var express = require('express')
 var app = express()
 var cors = require('cors')
+let token = ["eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFyYXZpbmQiLCJwYXNzd29yZCI6IjEyMzQ1In0.JemVFcxaHF-nL9TwfqKEn5Fr1wll8Y3X8bA32mPhb8A"] 
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,13 +17,18 @@ app.post('/v1/auth',post_auth)
 
 
 function get_weather_v1(request, response) {
+     let tokenn = req.query.tokenn
+     if (token.includes(tokenn)){
      response.json({"coord":{"lon":-123.262,"lat":44.5646},"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01n"}],"base":"stations","main":{"temp":282.61,"feels_like":282.61,"temp_min":280.58,"temp_max":285.29,"pressure":1018,"humidity":84},"visibility":10000,"wind":{"speed":0.89,"deg":225,"gust":0.89},"clouds":{"all":0},"dt":1642038331,"sys":{"type":2,"id":2040223,"country":"US","sunrise":1642002454,"sunset":1642035291},"timezone":-28800,"id":5720727,"name":"Corvallis","cod":200})
-}
+}}
 
 function get_hello(req,res){
     //res.set('content-type', 'application/json')
+    let tokenn = req.query.tokenn
+      if (token.includes(tokenn)){
+      
     res.json({"hello": "good evening"})
-}
+}}
 // const fs = require("fs");
 
 function post_auth(req,res){
@@ -34,7 +40,8 @@ function post_auth(req,res){
 
     if(usernames.includes(username)){
       if(passwords.includes(pwd)){
-          res.json({ "Sucess" : "Success, token generated"})
+          res.json({ "Sucess" : "Success, token generated","access-token":token,"expires":"2022-02-20T22:25:42.142Z"})
+           
       }
   }
 }
